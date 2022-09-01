d585df75c1684e05866ee04a03674190d0453ed3
dshandler-monash <dsha0047@student.monash.edu>
2022-09-01 23:54:51 +1000

    revert model

--- careworxdjango/service_list/models.py
+++ careworxdjango/service_list/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Model to access service_list TABLE in serviceListDB Database
-class serviceList(models.Model):
-    # … fields …
-    
-    class Meta:
-        managed = False
