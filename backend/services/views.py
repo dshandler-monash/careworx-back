c7fd5e1ef8086506d6db91755b16a407d5bc562d
dshandler-monash <dsha0047@student.monash.edu>
2022-09-05 21:19:46 +1000

    add service url

--- backend/services/views.py
+++ backend/services/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Service
+from .serializers import ServiceSerializer
+
+class ServiceDetailAPIView(generics.RetrieveAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    
+    #first lookup_field = 'pk'
+
+    #lookup_field = 'suburb'
+
+
+    #def get_queryset():
