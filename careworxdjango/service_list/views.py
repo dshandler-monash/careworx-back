880fd66599c65402c69a4f501f59a099cd3811b2
dshandler-monash <dsha0047@student.monash.edu>
2022-09-05 14:47:09 +1000

    creat views

--- careworxdjango/service_list/views.py
+++ careworxdjango/service_list/views.py
@@ -1,5 +1,13 @@
 from curses.ascii import HT
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import ServiceList
+from .serializers import ServiceListSerializers
 # Create your views here.
 
+def service_list(request):
+    # Get all services, serialize them and return json
+
+    services = ServiceList.objects.all()
+    serializer = ServiceListSerializers(services, many=True)
+    return JsonResponse(serializer.data, safe=False)
