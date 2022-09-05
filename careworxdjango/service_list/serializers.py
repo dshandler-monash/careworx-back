7d890dd7ac5b98f9008cb8d21b421a4bd74f916e
dshandler-monash <dsha0047@student.monash.edu>
2022-09-05 15:46:04 +1000

    serializer typo

--- careworxdjango/service_list/serializers.py
+++ careworxdjango/service_list/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import ServiceList
 
 
-class ServiceListSerializers(serializers.ModelSerializers):
+class ServiceListSerializers(serializers.ModelSerializer):
     class Meta:
         model = ServiceList
         fields = '__all__'
