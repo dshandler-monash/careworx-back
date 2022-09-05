bb5c372e306b1449938821240089cf03dd41b3eb
dshandler-monash <dsha0047@student.monash.edu>
2022-09-05 17:39:55 +1000

    add view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,20 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"Hi, here is a test response"})
+    # Generate JsonResponse from request
+    
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body) # takes string of json -> python dict
+    except:
+        pass
+    data['params'] = dict(request.GET) #request.GET gets URL query params
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content.type
+    
+    return JsonResponse(data)
