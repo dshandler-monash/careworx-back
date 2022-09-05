ad706d0ba6625f51878e01adc9b6a131aee8926c
dshandler-monash <dsha0047@student.monash.edu>
2022-09-05 23:18:56 +1000

    add product mixin view

--- backend/services/urls.py
+++ backend/services/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.ServiceListCreateAPIView.as_view()),
-    path('<int:pk>/',views.ServiceDetailAPIView.as_view())
+    path('',views.ProductMixinView.as_view()),
+    path('<int:pk>/',views.ProductMixinView.as_view())
 ]
--- backend/services/views.py
+++ backend/services/views.py
@@ -1,7 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.shortcuts import get_object_or_404
 
 from .models import Service
 from .serializers import ServiceSerializer
+from backend.services import serializers
 
 class ServiceCreateAPIView(generics.CreateAPIView):
     queryset = Service.objects.all()
@@ -11,9 +16,9 @@ class ServiceListCreateAPIView(generics.ListCreateAPIView):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer  
 
-class ServiceDetailAPIView(generics.RetrieveAPIView):
-    queryset = Service.objects.all()
-    serializer_class = ServiceSerializer
+# class ServiceDetailAPIView(generics.RetrieveAPIView):
+#     queryset = Service.objects.all()
+#     serializer_class = ServiceSerializer
     
     #first lookup_field = 'pk'
 
@@ -21,8 +26,39 @@ class ServiceDetailAPIView(generics.RetrieveAPIView):
 
 
     #def get_queryset():
+      
+# class ServiceListAPIView(generics.ListAPIView):
+#     queryset = Service.objects.all()
+#     serializer_class = ServiceSerializer
 
-        
-class ServiceListAPIView(generics.ListAPIView):
-    queryset = Service.objects.all()
+class ProductMixinView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+
+    qs = Service.objects.all() #queryset
     serializer_class = ServiceSerializer
+    lookup_field = 'pk' # change here for retrieveModelMixin
+
+    def get(self, request, *args, **kargs):
+        pk = kargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kargs)
+        return self.list(request, *args, **kargs)
+
+@api_view(['GET'])
+def product_alt_view(request, pk=None, *args, **kargs):
+    method = request.method
+
+    if method == "GET":
+        #detail view
+        if pk is not None:
+            #detail view
+            obj = get_object_or_404(Service, pk=pk) # gives object or http404
+            data = ServiceSerializer(obj, many=False).data
+            return Response(data)
+        #list view
+        qs = Service.objects.all() #queryset
+        data = ServiceSerializer(qs, many=True).data
+        return Response(data)
